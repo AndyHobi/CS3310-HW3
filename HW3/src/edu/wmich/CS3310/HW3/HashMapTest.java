cf0451deb5c652b4a5d053e6251bc0034e4696f6
Admin <Admin@10.80.147.101>
2019-02-28 19:25:11 -0500

    Hash Map Setup

--- HW3/src/edu/wmich/CS3310/HW3/HashMapTest.java
+++ HW3/src/edu/wmich/CS3310/HW3/HashMapTest.java
@@ -1,5 +1,44 @@
 package edu.wmich.CS3310.HW3;
 
+import java.util.LinkedList;
+
 public class HashMapTest {
 
+	String[] keys;
+	LinkedList<Integer>[] IDs;
+	String[] IDRef;
+	
+	int m; // This is the size of the Map
+	
+	
+	@SuppressWarnings("unchecked")
+	HashMapTest(int mapSize) {
+		
+		m = mapSize;
+		keys = new String[m];
+		IDs = new LinkedList[m];
+		IDRef = new String[10];	
+			
+	}
+	
+	public void add(String Data, String Key) {
+		
+		
+		
+	}
+	
+	public String search(String key) {
+		
+		return null;
+		
+	}
+	
+	public Integer CompareTo() {
+		return m;
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
 }
