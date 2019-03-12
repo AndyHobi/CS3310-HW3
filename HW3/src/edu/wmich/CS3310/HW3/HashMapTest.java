113dec84847569c3b06fb6124b48560a58f36dc9
Admin <Admin@10.80.144.15>
2019-03-12 13:37:41 -0400

    Big Update using sample file

--- HW3/src/edu/wmich/CS3310/HW3/HashMapTest.java
+++ HW3/src/edu/wmich/CS3310/HW3/HashMapTest.java
@@ -1,44 +1,109 @@
 package edu.wmich.CS3310.HW3;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class HashMapTest {
 
 	String[] keys;
-	LinkedList<Integer>[] IDs;
+	LinkedList<Integer>[] hashMap;
 	String[] IDRef;
-	
+
 	int m; // This is the size of the Map
-	
-	
-	@SuppressWarnings("unchecked")
+	int nextRef; // next index for IDRef
+
 	HashMapTest(int mapSize) {
-		
+
 		m = mapSize;
 		keys = new String[m];
-		IDs = new LinkedList[m];
-		IDRef = new String[10];	
-			
+		hashMap = new LinkedList[m];
+		IDRef = new String[100];
+		nextRef = 0;
+
 	}
-	
-	public void add(String Data, String Key) {
-		
-		
-		
+
+	public void add(String data, String... keys) {
+
+		IDRef[nextRef] = data;
+
+		for (String key : keys) {
+
+			int keyIndex = getIndex(key);
+
+			this.keys[keyIndex] = key;
+
+			if (hashMap[keyIndex] == null) {
+				hashMap[keyIndex] = new LinkedList<Integer>();
+			}
+
+			hashMap[keyIndex].add(nextRef);
+
+		}
+
+		nextRef++;
+
 	}
-	
-	public String search(String key) {
-		
-		return null;
-		
+
+	public String[] search(String key) {
+
+		return getItems(getIndex(key));
+
 	}
-	
-	public Integer CompareTo() {
+
+	public int CompareTo() {
 		return m;
 	}
-	
+
 	public String toString() {
 		return null;
 	}
-	
+
+	// ====== PRIVATE METHODS ======
+
+	private String[] getItems(int index) {
+
+		String[] returnArr = { "Nothing Found" };
+
+		if (hashMap[index] != null) {
+
+			Iterator<Integer> iter = hashMap[index].iterator();
+
+			returnArr = new String[hashMap[index].size()];
+			int j = 0;
+
+			while (iter.hasNext()) {
+
+				returnArr[j] = IDRef[iter.next()];
+				j++;
+			}
+
+		}
+
+		return returnArr;
+
+	}
+
+	private int getIndex(String key) { // using linear probing
+
+		int hash = Math.abs(key.hashCode()) % m;
+		int index = hash;
+
+		do {
+			String currKey = keys[index];
+			if (currKey == null || currKey.compareTo(key) == 0) {
+				return index;
+			} else {
+				index++;
+			}
+
+			if (index >= m) {
+				index = 0;
+			}
+
+		} while (index != hash);
+
+		return -1;
+
+	}
+
 }
--- HW3/src/edu/wmich/CS3310/HW3/Main.java
+++ HW3/src/edu/wmich/CS3310/HW3/Main.java
@@ -1,10 +1,40 @@
 package edu.wmich.CS3310.HW3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 
+		HashMapTest map = new HashMapTest(100); 
+		
+		File inFile = new File("sample.txt");
+		Scanner inScanner = new Scanner(inFile);
+		
+		Scanner kbd = new Scanner(System.in);
+		String input;
+		while(inScanner.hasNextLine()) {
+			
+			map.add(inScanner.nextLine(), inScanner.nextLine().split(" "));
+			
+		}
+		
+		while(true) {
+			System.out.println("Please ender your search term (or x to exit):");
+			input = kbd.nextLine();
+			
+			if(input.compareTo("x") == 0) {
+				break;
+			}
+			
+			System.out.println(map.search(input)[0]);
+			
+		}
+		
+		
 	}
 
 }
